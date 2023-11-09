4b89520725c484f6bc112b2b9dedf7d59483b1fa
OpenAst <isrealme0@gmail.com>
2023-11-09 12:41:22 -0800

    done

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,10 +9,10 @@ from flask_cors import (CORS, cross_origin)
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
-
 from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.session_db_auth import SessionDBAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -58,14 +58,15 @@ def authenticate_user():
     """
     if auth:
         excluded_paths = [
-            '/api/v1/status/',
-            '/api/v1/unauthorized/',
-            '/api/v1/forbidden/',
+            "/api/v1/status/",
+            "/api/v1/unauthorized/",
+            "/api/v1/forbidden/",
+            "/api/v1/auth_session/login/",
         ]
         if auth.require_auth(request.path, excluded_paths):
-            auth_header = auth.authorization_header(request)
             user = auth.current_user(request)
-            if auth_header is None:
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
                 abort(401)
             if user is None:
                 abort(403)
